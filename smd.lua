6815e1a6c9c4bfdabc12b141153a3430bb97ea37
reb <128338458+rebslayer@users.noreply.github.com>
2023-03-27 09:47:04 -0400

    Create smd.lua

---
+++ smd.lua
@@ -0,0 +1,7 @@
+game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
+  if inputObject.KeyCode == Enum.KeyCode.V then
+    local newpart = Instance.new("Part")
+    newpart.Parent = game.Workspace
+    newpart.Size = Udim3. -- i forgor
+  end
+end)
